675c1a9d30d8082ab9a6b67bcb53a5c6d0849166
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-07-13 16:45:32 +0200

    created card movie

--- index.php
+++ index.php
@@ -32,20 +32,43 @@ $transformers = new Movie('Transformers','Action', 'Michael Bay',[0,1], "Robot a
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Php oop 1</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <style>
-        img{
-    height: 500px;
-}
+        ul{
+            padding:0;
+            list-style-type:none;
+        }
+    </style>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+        <div class="card" style="width: 18rem;">
+             <img src="<?php echo $transformers->img; ?>" class="card-img-top" :alt="<?php $transformers->name ;?>">
+               <div class="card-body">
+               <h5 class="card-title"> <strong>Movie:</strong> <?php echo  $transformers->name ; ?></h5>
+               <p class="card-text"><strong>Genre: </strong><?php echo $transformers->genre; ?></p>
+               <p class="card-text"><strong>Director: </strong><?php echo $transformers->director; ?></p>
+               <ul>
+                <li>
+                   <strong>Actors: </strong> 
+                <?php foreach ($transformers->actors as $key => $actor) {
+                   echo $actor;
+                } ?>
+                </li>
+               </ul>
+               <p class="card-text"> <strong>Plot: </strong>  <?php echo $transformers->plot; ?></p>
+               <p class="card-text"> <strong>Budget: </strong>  <?php echo $transformers->budget . " USD" ;?></p>
+               <p class="card-text"> <strong>Best Actor: </strong>  <?php echo $transformers->getBestactors(); ?></p>
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
 
-</style>
-
-<?php
-    
-echo "<p>Movie: " . $transformers->name . "</p>";
-echo "<p>Genre: " . $transformers->genre . "</p>";
+<!-- echo "<p>Genre: " . $transformers->genre . "</p>";
 echo "<p>Director: " . $transformers->director . "</p>";
 foreach ($transformers->actors as $key => $actor) {
     echo "<span>" . $actor . "</span>";
@@ -53,8 +76,7 @@ foreach ($transformers->actors as $key => $actor) {
 echo "<p>Plot: " . $transformers->plot . "</>";
 echo "<p>Budget: " . $transformers->budget . " USD" . "</p>";
 echo "<p>Best Actor: " . $transformers->getBestactors() . "</p>";
-echo "<img src='$transformers->img'>";    
-    ?>
+echo "<img src='$transformers->img'>";     -->
 
 </body>
 </html>
